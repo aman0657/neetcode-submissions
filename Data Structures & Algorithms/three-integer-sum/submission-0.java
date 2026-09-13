742eda2ad85b82325723c2dd19d89ef85be6ecd3
Aman Kumar <76102618+aman0657@users.noreply.github.com>
2026-09-13 17:36:25 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        Set<List<Integer>> output = new HashSet<>();
+        for(int i=0;i<nums.length-2;i++){
+            int j = i + 1;
+            int k = n - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0){
+                    output.add(List.of(nums[i], nums[j], nums[k]));
+                    j ++;
+                    k --;
+                } else if (sum < 0){
+                    j ++; 
+                } else {
+                    k --;
+                }
+            }
+        }
+        return new ArrayList<>(output);
+    }
+}
